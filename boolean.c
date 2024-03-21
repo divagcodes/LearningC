9f563af8b460cdc7ca29345d6fdb2f51572f5f48
divagcodes <divineagbeleye@gmail.com>
2024-03-21 19:46:23 +0000

    Update changes

--- boolean.c
+++ boolean.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+
 int main() {
 
     bool value = ( 9 < 10 );
---
+++ programmingproject.c
@@ -0,0 +1,244 @@
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+  int i;
+  int ans1,ans2,ans3,ans4,ans5,ans6,ans7,ans8,ans9,ans10;
+  int point1,point2,point3,point4,point5,point6,point7,point8,point9,point10;
+  int point01,point02,point03,point04,point05,point06,point07,point08,point09,point010;
+  int total1,total2,total3;
+  printf("welcome to the game\n");
+  
+  printf(">press 7 to Start The Game\n");
+  printf(">press 0 to end the Game\n");
+  
+  scanf("%d", &i);
+   
+   if(i==7)
+   {
+       printf("The Game has started\n\n");
+   }
+   else if (i==0)
+   {
+        printf ("the game has ended\n\n");
+   }
+   else
+   {
+        printf("invalid\n");
+   }
+   if(i==7)
+   {   
+        printf("1)who is the avatar in the movie Avatar the last Airbender\n\n");
+    printf("1)kitara\n");
+    printf("2)aang\n");
+    printf("3)zuko\n");
+    printf("4)toph\n");
+    
+    printf("enter your answer :");
+    scanf("%d", &ans1);
+    
+    if(ans1==2)
+    {
+        printf("correct answer\n\n");
+        point1=5;
+        printf("you have scored %d points\n\n", point1);
+    }
+    else
+    {
+        printf("Wrong answer");
+        point01=0;
+        printf("you have scored %d point\n" , point01);
+    }
+           printf("2)what was the name of aangs bison\n\n");
+    printf("1)momo\n");
+    printf("2)appa\n");
+    printf("3)ruko\n");
+    printf("4)tatsu\n");
+    
+    printf("enter your answer :");
+    scanf("%d", &ans2);
+    
+    if(ans2==2)
+    {
+        printf("correct answer\n\n");
+        point2=5;
+        printf("you have scored %d points\n\n", point2);
+    }
+    else
+    {
+        printf("Wrong answer");
+        point02=0;
+        printf("you have scored %d point\n" , point02);
+    }
+           printf("3)which out of the following team avatar member was not a bender\n\n");
+    printf("1)kitara\n");
+    printf("2)toph\n");
+    printf("3)zuko\n");
+    printf("4)sokka\n");
+    
+    printf("enter your answer :");
+    scanf("%d", &ans3);
+    
+    if(ans3==4)
+    {
+        printf("correct answer\n\n");
+        point3=5;
+        printf("you have scored %d points\n\n", point3);
+    }
+    else
+    {
+        printf("Wrong answer");
+        point03=0;
+        printf("you have scored %d point\n" , point03);
+    }
+           printf("4)which element did aang originally refuse to bend\n\n");
+    printf("1)water\n");
+    printf("2)earth\n");
+    printf("3)fire\n");
+    printf("4)air\n");
+    
+    printf("enter your answer :");
+    scanf("%d", &ans4);
+    
+    if(ans4==3)
+    {
+        printf("correct answer\n\n");
+        point4=5;
+        printf("you have scored %d points\n\n", point4);
+    }
+    else
+    {
+        printf("Wrong answer");
+        point04=0;
+        printf("you have scored %d point\n" , point04);
+    }
+           printf("5)which was the hardest element for aang to bend\n\n");
+    printf("1)air\n");
+    printf("2)earth\n");
+    printf("3)fire\n");
+    printf("4)water\n");
+    
+    printf("enter your answer :");
+    scanf("%d", &ans5);
+    
+    if(ans5==2)
+    {
+        printf("correct answer\n\n");
+        point5=5;
+        printf("you have scored %d points\n\n", point5);
+    }
+    else
+    {
+        printf("Wrong answer");
+        point05=0;
+        printf("you have scored %d point\n" , point05);
+    }
+           printf("6)who fire bends blue flames\n\n");
+    printf("1)zuko\n");
+    printf("2)azula\n");
+    printf("3)ruko\n");
+    printf("4)iroh\n");
+    
+    printf("enter your answer :");
+    scanf("%d", &ans6);
+    
+    if(ans6==2)
+    {
+        printf("correct answer\n\n");
+        point6=5;
+        printf("you have scored %d points\n\n", point6);
+    }
+    else
+    {
+        printf("Wrong answer");
+        point06=0;
+        printf("you have scored %d point\n" , point06);
+    }
+           printf("7)which of the first nations where wiped by the fire nation during the first war\n\n");
+    printf("1)water tribe\n");
+    printf("2)fire nation\n");
+    printf("3)white locust\n");
+    printf("4)air normads\n");
+    
+    printf("enter your answer :");
+    scanf("%d", &ans7);
+    
+    if(ans7==4)
+    {
+        printf("correct answer\n\n");
+        point7=5;
+        printf("you have scored %d points\n\n", point7);
+    }
+    else
+    {
+        printf("Wrong answer");
+        point07=0;
+        printf("you have scored %d point\n" , point07);
+    }
+           printf("8)which avatar number ia aang\n\n");
+    printf("1)82nd\n");
+    printf("2)52th\n");
+    printf("3)78th\n");
+    printf("4)65th\n");
+    
+    printf("enter your answer :");
+    scanf("%d", &ans8);
+    
+    if(ans8==1)
+    {
+        printf("correct answer\n\n");
+        point8=5;
+        printf("you have scored %d points\n\n", point8);
+    }
+    else
+    {
+        printf("Wrong answer");
+        point08=0;
+        printf("you have scored %d point\n" , point08);
+    }
+           printf("9)what was sokkas first weapon in the series\n\n");
+    printf("1)sword\n");
+    printf("2)matchette\n");
+    printf("3)boomerange\n");
+    printf("4)club\n");
+    
+    printf("enter your answer :");
+    scanf("%d", &ans9);
+    
+    if(ans9==3)
+    {
+        printf("correct answer\n\n");
+        point9=5;
+        printf("you have scored %d points\n\n", point9);
+    }
+    else
+    {
+        printf("Wrong answer");
+        point09=0;
+        printf("you have scored %d point\n" , point09);
+    }
+           printf("10)which kind of creature gave aang the power of energybending\n\n");
+    printf("1)a giant owl\n");
+    printf("2)a lion turtle\n");
+    printf("3)momo\n");
+    printf("4)a polar bear\n");
+    
+    printf("enter your answer :");
+    scanf("%d", &ans10);
+    
+    if(ans10==2)
+    {
+        printf("correct answer\n\n");
+        point10=5;
+        printf("you have scored %d points\n\n", point10);
+    }
+    else
+    {
+        printf("Wrong answer");
+        point010=0;
+        printf("you have scored %d point\n" , point010);
+    }
+   }
+    return 0;
+}
