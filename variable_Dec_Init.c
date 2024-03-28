0f6c9a5aa4df27e357c0ba7c5b578bf217b99765
AgVicCodes <victoragbeleye2@gmail.com>
2024-03-28 10:33:08 +0000

    Added addition problem

---
+++ variable_Dec_Init.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+    // variable declaration
+    int a;
+
+    // variable initialization
+    a = 5;
+
+    // Inline delaration and initialization
+    int a = 5;
+    
+    return 0;
+    
+}
